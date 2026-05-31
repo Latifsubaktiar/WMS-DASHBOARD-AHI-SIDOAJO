403d9c67efdffa0b7206abe86945fc1b13f6afa1
Latifsubaktiar <mochammad.subaktiar@kawanlamacorp.com>
2026-05-31 23:24:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1991,7 +1991,7 @@ async function fetchDailyActivity(){
             </tbody>
           </table>
         </div>`;
-      floatBox.style.cssText = 'position:absolute;top:-48px;left:160px;z-index:10;';
+      floatBox.style.cssText = 'position:absolute;top:6px;left:160px;z-index:10;';
       wrap.appendChild(floatBox);
     }
 
