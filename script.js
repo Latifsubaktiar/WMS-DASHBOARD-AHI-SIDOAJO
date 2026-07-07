afc4febaed96f0b3c98b02b3aba62a608f82781e
Latifsubaktiar <mochammad.subaktiar@kawanlamacorp.com>
2026-07-07 09:18:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const GAS_DASHBOARD_URL = 'https://script.google.com/macros/s/AKfycbxxjijcpvbfzK
 
 const URLS = {
   planner:   'https://plannerazko.github.io/DashboardPlanning/',
-  inventory: 'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbznK9XTBd5Zz07tyb-1bvjQEa00pXEMPFOXhCtYaBqthThQUliRjcXUYYr27VaXV-888w/exec',
+  inventory: 'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbwgrzzsmS8Nab4WhTLIOdFiknXfV4Ak-jE8Ewd9qBvR_uh93c2VBa3OInHAzbEI8HrdUw/exec',
   outbound:  'https://outboundazko.github.io/Monitoring-Loading/index.html',
   analyst:   'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbzbsvn8Tiu3N3WGjBRbN_6-CsqAI9vTl2IxW1bsYi92Gk15Alzzk1JBvL4iyyvnL8nj/exec',
   inbound:   'https://script.google.com/macros/s/AKfycbyuWCeC7vR3JrVbDu9wn99FCtWl9oN1hZ3pJ5Kbrx9sOXg_l9xjoIAu2qTZMSulYWrb/exec', storing: 'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbyBTR01ZrItlvxh1C83yxFs5B8lwK5t-a4kuKHir8s8a1SprmUA_6TsRkzDZpWOaBL9Ew/exec',
