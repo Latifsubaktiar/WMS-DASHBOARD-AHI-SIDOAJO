7cb62e6746b35ee6d9e086cc9024474d2f7d544a
Latifsubaktiar <mochammad.subaktiar@kawanlamacorp.com>
2026-09-19 05:57:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const URLS = {
   analyst:   'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbzbsvn8Tiu3N3WGjBRbN_6-CsqAI9vTl2IxW1bsYi92Gk15Alzzk1JBvL4iyyvnL8nj/exec',
   inbound:   'https://script.google.com/macros/s/AKfycbyXFO0rz1pQeRzT8ZBk6v1AHxcbvw4iqsuYcFxLIa_f6Sj5XrDncYUVkkE5RwHj_TAYZA/exec', storing: 'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbyBTR01ZrItlvxh1C83yxFs5B8lwK5t-a4kuKHir8s8a1SprmUA_6TsRkzDZpWOaBL9Ew/exec',
   ga:        'https://script.google.com/macros/s/AKfycbzAKPAl_-Bb36LP1qAXgK1DRaYqxz2GUP_4-sbkGHpkxdmzIU4BlaPBYhUvvi04EV7d/exec',
-  hr:        null,
+  hr:        'https://script.google.com/a/macros/kawanlamacorp.com/s/AKfycbyNQYijkh5bA5TBLY33OYgWxzyGlhKmqWs0qHowjBthmU1xO3nRA7mtHEbMa5Z-yOgt/exec',
 };
 const IFRAME_PAGES   = ['profildc','inventory','outbound','planner','ga','analyst','storing','inbound'];
 const GAS_AI_URL     = 'https://script.google.com/macros/s/AKfycbxxjijcpvbfzKtZH1gJKPswP1heNpopp2TERUESg5mJiLu7t8qZuSpVist4uAMwxZzN/exec';
